cd819d04aca8add98e2e836afc2ed6f4070d9f07
David Heath <david.heath@digital.cabinet-office.gov.uk>
2023-12-01 13:52:52 +0000

    make invokable and add puzzle input

--- lib/day1.rb
+++ lib/day1.rb
@@ -6,10 +6,18 @@ class Day1
 
 	def line_calibration_value(line)
 		first,last = find_first_and_last_digits_of_line(line)
-		first * 10 + last
+		if first
+			first * 10 + last
+		else
+			0
+		end
 	end
 
 	def calibration_value(input)
 		input.split("\n").reject(&:empty?).map {|l| line_calibration_value(l)}.sum
 	end
+end
+
+if __FILE__==$0
+	puts Day1.new.calibration_value(ARGF.read)
 end
