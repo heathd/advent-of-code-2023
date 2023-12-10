78b1e14385f35e269d5361b4cb9cf22ba26be962
David Heath <david.heath@digital.cabinet-office.gov.uk>
2023-12-10 17:40:02 +0000

    Day5

---
+++ lib/day5.rb
@@ -0,0 +1,155 @@
+class Mapper
+	attr_reader :map_data, :map_builder
+
+	def initialize(map_data, map_builder: MapBuilder.new)
+		@map_data = map_data
+		@map_builder = map_builder
+		@maps = parse_maps
+	end
+
+	def seeds
+		map_data.match(/seeds: (([0-9]+ )*[0-9]+)$/)
+		$1.split(" ").map(&:to_i)
+	end
+
+	def maps
+		@maps ||= parse_maps
+	end
+
+	def parse_maps
+		_, *maps = map_data.split("\n\n")
+		maps.map {|m| map_builder.parse(m.strip + "\n") }.flatten
+	end
+
+	def map(from, num)
+		maps.each do |m| 
+			dest = m.map(from, num)
+			return dest if dest != nil
+		end
+
+		nil
+	end
+
+	def mapping_path(seed)
+		type = :seed
+		num = seed
+
+		path = [[type, num]]
+		while mapped = map(type, num)
+			path << mapped
+			type,num = mapped
+		end
+		path
+	end
+
+	def ultimate_mapping(seed)
+		mapping_path(seed).last.last
+	end
+
+	def mappings_for_seeds
+		seeds.map { |seed| ultimate_mapping(seed) }
+	end
+
+	def lowest_mapping_for_seeds
+		mappings_for_seeds.min
+	end
+end
+
+class MapBuilder
+	def parse(map_data)
+		scanner = StringScanner.new(map_data)
+		scanner.scan_until(/(.*?)-to-(.*?) map:/)
+		from_type, to_type = scanner.captures.map(&:to_sym)
+
+		map_for_type = MapForType.new(from_type, to_type)
+
+		while scanner.scan_until(/([0-9]+) ([0-9]+) ([0-9]+)/)
+			d, s, l = scanner.captures.map(&:to_i)
+			map_for_type.add_mapping!(d, s, l)
+		end
+
+		map_for_type
+	end
+end
+
+class Map
+	ATTRS = %I{from_type to_type destination_range_start source_range_start length}
+	attr_reader *ATTRS
+
+	include Comparable
+
+	def initialize(from_type, to_type, destination_range_start, source_range_start, length)
+		@from_type = from_type
+		@to_type = to_type
+		@destination_range_start = destination_range_start
+		@source_range_start = source_range_start
+		@length = length
+	end
+
+	def can_map?(from)
+		 from == @from_type
+	end
+
+	def in_range?(num)
+		num >= @source_range_start && num < @source_range_start + @length
+	end
+
+	def map(from, num)
+		if can_map?(from) && in_range?(num)
+			[@to_type, @destination_range_start + num - @source_range_start]
+		else
+			nil
+		end
+	end
+
+	def <=>(other)
+		self_data = ATTRS.map {|a| [a, self.send(a)]}
+		other_data = ATTRS.map {|a| [a, other.send(a)]}
+		self_data <=> other_data
+	end
+end
+
+class MapForType
+	ATTRS = %I{from_type to_type maps}
+	attr_reader *ATTRS
+
+	include Comparable
+
+	def initialize(from_type, to_type)
+		@from_type = from_type
+		@to_type = to_type
+		@maps = []
+	end
+
+	def add_mapping!(destination_range_start, source_range_start, length)
+		@maps << Map.new(from_type, to_type, destination_range_start, source_range_start, length)
+	end
+
+	def can_map?(from)
+		 from == @from_type
+	end
+
+	def in_range?(num)
+		@maps.any? {|m| m.in_range?(num)}
+	end
+
+	def map(from, num)
+		if can_map?(from)
+			map = @maps.find { |m| m.in_range?(num) }
+			if map
+				map.map(from, num)
+			else
+				[@to_type, num]
+			end
+		else
+			nil
+		end
+	end
+
+	def <=>(other)
+		self_data = ATTRS.map {|a| [a, self.send(a)]}
+		other_data = ATTRS.map {|a| [a, other.send(a)]}
+		self_data <=> other_data
+	end
+end
+
---
+++ spec/day5_spec.rb
@@ -0,0 +1,212 @@
+require 'day5'
+
+RSpec.describe Mapper do
+	let(:example) { <<~EXAMPLE
+		seeds: 79 14 55 13
+
+		seed-to-soil map:
+		50 98 2
+		52 50 48
+
+		soil-to-fertilizer map:
+		0 15 37
+		37 52 2
+		39 0 15
+
+		fertilizer-to-water map:
+		49 53 8
+		0 11 42
+		42 0 7
+		57 7 4
+
+		water-to-light map:
+		88 18 7
+		18 25 70
+
+		light-to-temperature map:
+		45 77 23
+		81 45 19
+		68 64 13
+
+		temperature-to-humidity map:
+		0 69 1
+		1 0 69
+
+		humidity-to-location map:
+		60 56 37
+		56 93 4
+		EXAMPLE
+	}
+
+	subject(:mapper) { described_class.new(example) }
+
+	it "parses the seeds" do
+		expect(mapper.seeds).to eq([79,14,55,13])
+	end
+
+	describe "interaction with MapBuilder" do
+		let(:map_builder) { instance_double(MapBuilder, parse: nil) }
+		subject(:mapper) { described_class.new(example, map_builder: map_builder) }
+
+		it "parses the maps" do
+			mapper
+
+			expect(map_builder).to have_received(:parse).with <<~DATA
+				seed-to-soil map:
+				50 98 2
+				52 50 48
+			DATA
+
+			expect(map_builder).to have_received(:parse).with <<~DATA
+				soil-to-fertilizer map:
+				0 15 37
+				37 52 2
+				39 0 15
+			DATA
+
+			expect(map_builder).to have_received(:parse).with <<~DATA
+				fertilizer-to-water map:
+				49 53 8
+				0 11 42
+				42 0 7
+				57 7 4
+			DATA
+
+			expect(map_builder).to have_received(:parse).with <<~DATA
+				water-to-light map:
+				88 18 7
+				18 25 70
+			DATA
+
+			expect(map_builder).to have_received(:parse).with <<~DATA
+				light-to-temperature map:
+				45 77 23
+				81 45 19
+				68 64 13
+			DATA
+
+			expect(map_builder).to have_received(:parse).with <<~DATA
+				temperature-to-humidity map:
+				0 69 1
+				1 0 69
+			DATA
+
+			expect(map_builder).to have_received(:parse).with <<~DATA
+				humidity-to-location map:
+				60 56 37
+				56 93 4
+			DATA
+		end
+	end
+
+	describe "#map" do
+		it "maps a specific from/to type and number" do
+			expect(mapper.map(:light, 77)).to eq([:temperature, 45])
+			expect(mapper.map(:soil, 15)).to eq([:fertilizer, 0])
+		end
+
+		it "maps to the destination type with the same value if there's no mapping" do
+			expect(mapper.map(:soil, 77)).to eq([:fertilizer, 77])
+		end
+
+		it "maps to nil if the mapping is out of range for the type" do
+			expect(mapper.map(:light, 44)).to eq([:temperature, 44])
+		end
+
+		it "maps the whole chain for the first example" do
+			expect(mapper.map(:seed, 79)).to eq([:soil, 81])
+			expect(mapper.map(:soil, 81)).to eq([:fertilizer, 81])
+			expect(mapper.map(:fertilizer, 81)).to eq([:water, 81])
+			expect(mapper.map(:water, 81)).to eq([:light, 74])
+			expect(mapper.map(:light, 74)).to eq([:temperature, 78])
+			expect(mapper.map(:temperature, 78)).to eq([:humidity, 78])
+			expect(mapper.map(:humidity, 78)).to eq([:location, 82])
+			# :location, 82
+		end
+	end
+
+	describe "#ultimate_mapping" do
+		it "maps 79 ultimately to 82" do
+			expect(mapper.ultimate_mapping(79)).to eq(82)
+		end
+	end
+
+	describe "mapping for seeds" do
+		it "finds all the mappings" do
+			expect(mapper.mappings_for_seeds).to eq([82, 43, 86, 35])
+		end
+
+		it "finds the lowest mapping" do
+			expect(mapper.lowest_mapping_for_seeds).to eq(35)
+		end
+	end
+end
+
+RSpec.describe Map do
+	subject(:map) { Map.new(:seed, :soil, 50, 98, 3) }
+
+	it "maps numbers before the range to nil" do
+		expect(map.map(:seed, 97)).to eq(nil)
+	end
+
+	it "maps the number at the start of the range" do
+		expect(map.map(:seed, 98)).to eq([:soil, 50])
+	end
+
+	it "maps the number at the end of the range" do
+		expect(map.map(:seed, 100)).to eq([:soil, 52])
+	end
+
+	it "maps the number just after the end of the range to nil" do
+		expect(map.map(:seed, 101)).to eq(nil)
+	end
+end
+
+RSpec.describe MapForType do
+	subject(:map_for_type) { MapForType.new(:seed, :soil) }
+
+	context "a single mapping" do
+		before(:each) {
+			map_for_type.add_mapping!(50, 98, 3)
+		}
+
+		it "maps numbers before the range to the same number in the destination range" do
+			expect(map_for_type.map(:seed, 97)).to eq([:soil, 97])
+		end
+
+		it "maps the number at the start of the range" do
+			expect(map_for_type.map(:seed, 98)).to eq([:soil, 50])
+		end
+
+		it "maps the number at the end of the range" do
+			expect(map_for_type.map(:seed, 100)).to eq([:soil, 52])
+		end
+
+		it "maps the number just after the end of the range to the same number in the destination range" do
+			expect(map_for_type.map(:seed, 101)).to eq([:soil, 101])
+		end
+
+		it "returns nil for a type not mapped" do
+			expect(map_for_type.map(:land, 101)).to eq(nil)
+		end
+	end
+end
+
+RSpec.describe MapBuilder do
+	subject(:map_builder) { MapBuilder.new }
+
+	let(:data) {
+		<<~DATA
+		seed-to-soil map:
+		50 98 2
+		52 50 48
+		DATA
+	}
+
+	it "parses the data" do
+		expected = MapForType.new(:seed, :soil)
+		expected.add_mapping!(50, 98, 2)
+		expected.add_mapping!(52, 50, 48)
+		expect(map_builder.parse(data)).to eq(expected)
+	end
+end
