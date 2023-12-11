4741b64f395bd07b66750aa3c5dda00a1f13e390
David Heath <david.heath@digital.cabinet-office.gov.uk>
2023-12-11 23:19:29 +0000

    Move CamelCards impl to own file

--- spec/day7_spec.rb
+++ spec/day7_spec.rb
@@ -1,28 +1,5 @@
 require 'day7/hand'
-
-class CamelCards
-  def initialize
-    @hands = []
-  end
-
-  def add_hand(hand, bid)
-    @hands << {
-      hand: Hand.new(hand),
-      bid: bid
-    }
-  end
-
-  def rank
-    @hands.map {|h| h[:hand]}.sort
-  end
-
-  def total_winnings
-    sorted = @hands.sort_by {|h| h[:hand]}.reverse
-    sorted.each.with_index.inject(0) do |memo, (hand_rec, i)|
-      memo + hand_rec[:bid] * (i+1)
-    end
-  end
-end
+require 'day7/camel_cards'
 
 RSpec.describe CamelCards do
   subject(:game) { described_class.new }
