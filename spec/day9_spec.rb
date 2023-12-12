b72f6091d15687bf4145f9a95ce1a6c4e7bc4e9f
David Heath <david.heath@digital.cabinet-office.gov.uk>
2023-12-12 22:05:21 +0000

    convert tabs to spaces

--- spec/day9_spec.rb
+++ spec/day9_spec.rb
@@ -1,28 +1,28 @@
 require 'day9'
 
 RSpec.describe "Day9" do
-	describe '.differential' do
-		it "calculates the first order differential of a sequence of numbers" do
-			expect(SequenceAnalyzer.differential([0,3,6,9,12,15])).to eq([3,3,3,3,3])
-		end
-	end
+  describe '.differential' do
+    it "calculates the first order differential of a sequence of numbers" do
+      expect(SequenceAnalyzer.differential([0,3,6,9,12,15])).to eq([3,3,3,3,3])
+    end
+  end
 
-	describe SequenceAnalyzer do
-		subject(:sequence_analyzer) { described_class.new(sequence) }
+  describe SequenceAnalyzer do
+    subject(:sequence_analyzer) { described_class.new(sequence) }
 
-		context "given a sequence" do
-			let(:sequence) { [0,3,6,9,12,15] }
+    context "given a sequence" do
+      let(:sequence) { [0,3,6,9,12,15] }
 
-			it "finds all of the differentials until the diff is zero" do
-				expect(sequence_analyzer.differentials).to eq([
-					[3,3,3,3,3],
-					[0,0,0,0]
-				])
-			end
+      it "finds all of the differentials until the diff is zero" do
+        expect(sequence_analyzer.differentials).to eq([
+          [3,3,3,3,3],
+          [0,0,0,0]
+        ])
+      end
 
-			it "predicts the next in the sequence" do
-				expect(sequence_analyzer.extrapolate).to eq(18)
-			end
-		end
-	end
+      it "predicts the next in the sequence" do
+        expect(sequence_analyzer.extrapolate).to eq(18)
+      end
+    end
+  end
 end
