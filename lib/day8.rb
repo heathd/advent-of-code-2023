e5c2c5bb3767f8267aaf56d08c4c17fbab377316
David Heath <david.heath@digital.cabinet-office.gov.uk>
2023-12-12 21:26:42 +0000

    This one was tough
    I had a hunch that you'd need to work out the periodicity of each sequence.
    Tried brute forcing but that seemed to be taking too long.
    
    I inspected the cycle times manually and was curious what the prime
    factorisations would be.
    
    Haven't deeply thought it through but each cycle I have has the length of the
    instruction sequence (281) as one prime factor and something else as the other
    prime factor.
    
    I got to the result mainly by guesswork that it would be some multiple of the
    prime factors :shrug:

--- lib/day8.rb
+++ lib/day8.rb
@@ -1,3 +1,6 @@
+require 'prime'
+require 'logger'
+
 class Navigator
   attr_reader :input, :lines
 
@@ -16,16 +19,6 @@ class Navigator
     i.chars.all? {|c| %w{L R}.include?(c)}
   end
 
-  def instructions_enumerator
-    Enumerator.new do |enum|
-      i = 0
-      while true
-        enum.yield(instructions[i % instructions.size])
-        i += 1
-      end
-    end
-  end
-
   def nodes
     @nodes ||= begin
       as_array = lines[2..-1].map do |line|
@@ -37,12 +30,17 @@ class Navigator
   end
 
   def path_to_zzz
-    i = instructions_enumerator
+    path_to(start_node: "AAA", pred: lambda {|n| n == 'ZZZ'})
+  end
+
+  def path_to(start_node:, pred: )
+    i = 0
     path = []
 
-    current = "AAA"
-    while current != "ZZZ"
-      instruction = i.next
+    current = start_node
+    while !pred.call(current)
+      instruction = instructions[i % instructions.size]
+      i+=1
       current = if instruction == "R"
         nodes[current][1]
       elsif instruction == "L"
@@ -54,8 +52,70 @@ class Navigator
     end
     path
   end
+
+  def path_to_node_ending_z(start_node)
+    path_to(start_node: start_node, pred: lambda {|n| n[-1]=="Z"})
+  end
+end
+
+class SequenceFromStartNode
+  attr_reader :start_node, :navigator, :path_since_start
+  attr_accessor :pos
+
+  def initialize(start_node, navigator)
+    @start_node = start_node
+    @current_node = start_node
+    @navigator = navigator
+    @pos = 0
+    @path_since_start = []
+  end
+
+  def path_to_z
+    path = navigator.path_to_node_ending_z(start_node)
+    @path_since_start += path
+    @current_node = path.last
+    @pos += path.size
+    path
+  end
+end
+
+class GhostNavigator
+  attr_reader :navigator, :logger
+
+  def initialize(input, logger: Logger.new(nil))
+    @navigator = Navigator.new(input)
+    @logger = logger
+  end
+
+  def starting_points
+    navigator.nodes.keys.select {|k| k[-1] == "A"}
+  end
+
+  def navigators
+    @navigators ||= starting_points.map {|s| SequenceFromStartNode.new(s, navigator) }
+  end
+
+  def steps_to_all_be_at_z
+    logger.info "Finding cycle lengths..."
+    cycle_lengths = navigators.map.with_index do |n, i| 
+      size = n.path_to_z.size
+      logger.info "#{i}: #{size}"
+      size
+    end
+
+    prime_factors = cycle_lengths.map {|n| ::Prime.prime_division(n)}.flatten.uniq
+
+    logger.info "Prime factors: #{prime_factors.join(', ')}"
+    prime_factors.inject(1, &:*)
+  end
+
+  def all_at_same_position?(positions)
+    positions.group_by {|p| p}.size == 1
+  end
 end
 
 if __FILE__==$0
-  puts Navigator.new(ARGF.read).path_to_zzz.size
+  logger = Logger.new($stdout)
+  result = GhostNavigator.new(ARGF.read, logger: logger).steps_to_all_be_at_z
+  logger.info "Result: #{result}"
 end
--- spec/day8_spec.rb
+++ spec/day8_spec.rb
@@ -1,66 +1,169 @@
+require 'day8'
+
 RSpec.describe Navigator do
-	it "can parse instructions" do
-		expect(Navigator.new("RL").instructions).to eq("RL")
-		expect(Navigator.new("R").instructions).to eq("R")
-	end
+  it "can parse instructions" do
+    expect(Navigator.new("RL").instructions).to eq("RL")
+    expect(Navigator.new("R").instructions).to eq("R")
+  end
 
-	it "rejects illegal instructions" do
-		expect {Navigator.new(" ").instructions}.to raise_error("Illegal instruction")
-	end
+  it "rejects illegal instructions" do
+    expect {Navigator.new(" ").instructions}.to raise_error("Illegal instruction")
+  end
 
-	it "can parse nodes" do
-		expect(Navigator.new("RL\n\nAAA = (BBB, CCC)\n").nodes).to eq({"AAA" => ["BBB", "CCC"]})
-	end
+  it "can parse nodes" do
+    expect(Navigator.new("RL\n\nAAA = (BBB, CCC)\n").nodes).to eq({"AAA" => ["BBB", "CCC"]})
+  end
 
-	context "the example given in the exercise description" do
-		subject(:navigator) {described_class.new(input)}
-		
-		let(:input) {
-			<<~MAP
-			RL
+  context "the example given in the exercise description" do
+    subject(:navigator) {described_class.new(input)}
+    
+    let(:input) {
+      <<~MAP
+      RL
 
-			AAA = (BBB, CCC)
-			BBB = (DDD, EEE)
-			CCC = (ZZZ, GGG)
-			DDD = (DDD, DDD)
-			EEE = (EEE, EEE)
-			GGG = (GGG, GGG)
-			ZZZ = (ZZZ, ZZZ)
-			MAP
-		}
+      AAA = (BBB, CCC)
+      BBB = (DDD, EEE)
+      CCC = (ZZZ, GGG)
+      DDD = (DDD, DDD)
+      EEE = (EEE, EEE)
+      GGG = (GGG, GGG)
+      ZZZ = (ZZZ, ZZZ)
+      MAP
+    }
 
-		it "can follow instructions until it reaches ZZZ" do
-			expect(navigator.path_to_zzz).to eq(%w{
-				CCC
-				ZZZ
-			})
-		end
-	end
+    it "can follow instructions until it reaches ZZZ" do
+      expect(navigator.path_to_zzz).to eq(%w{
+        CCC
+        ZZZ
+      })
+    end
+  end
 
-	context "a second example" do
-		subject(:navigator) {described_class.new(input)}
-		
-		let(:input) {
-			<<~MAP
-			LLR
+  context "a second example" do
+    subject(:navigator) {described_class.new(input)}
+    
+    let(:input) {
+      <<~MAP
+      LLR
 
-			AAA = (BBB, BBB)
-			BBB = (AAA, ZZZ)
-			ZZZ = (ZZZ, ZZZ)
-			MAP
-		}
+      AAA = (BBB, BBB)
+      BBB = (AAA, ZZZ)
+      ZZZ = (ZZZ, ZZZ)
+      MAP
+    }
 
-		it "can follow instructions until it reaches ZZZ" do
-			expect(navigator.path_to_zzz).to eq(%w{
-				BBB
-				AAA
-				BBB
-				AAA
-				BBB
-				ZZZ
-			})
-		end
+    it "can follow instructions until it reaches ZZZ" do
+      expect(navigator.path_to_zzz).to eq(%w{
+        BBB
+        AAA
+        BBB
+        AAA
+        BBB
+        ZZZ
+      })
+    end
 
-	end
+  end
+
+end
+
+RSpec.describe SequenceFromStartNode do
+  subject(:seq) {described_class.new(start_node, navigator)}
+  subject(:navigator) {Navigator.new(input)}
+    
+  let(:input) {
+    <<~MAP
+      LR
+
+      11A = (11B, XXX)
+      11B = (XXX, 11Z)
+      11Z = (11B, XXX)
+      22A = (22B, XXX)
+      22B = (22C, 22C)
+      22C = (22Z, 22Z)
+      22Z = (22B, 22B)
+      XXX = (XXX, XXX)
+    MAP
+  }
+
+  context "starting at 11A" do
+    let(:start_node) {"11A"}
+
+    it "position starts at zero" do
+      expect(seq.pos).to eq(0)
+    end
+
+    it "describes the path to a node ending Z" do
+      expect(seq.path_to_z).to eq(%w{
+        11B
+        11Z
+      })
+    end
+
+    it "advances pos to 2 after consuming two elements of a sequence" do
+      seq.path_to_z
+      expect(seq.pos).to eq(2)
+    end
+
+    it "advances to the next Z when called a second time" do
+      seq.path_to_z
+      expect(seq.path_to_z).to eq(%w{
+        11B
+        11Z
+      })
+      expect(seq.pos).to eq(4)
+    end
+  end
+
+  context "starting at 11A" do
+    let(:start_node) {"22A"}
+
+    it "describes the path to a node ending Z" do
+      expect(seq.path_to_z).to eq(%w{
+        22B
+        22C
+        22Z
+      })
+    end
+
+    it "advances pos to 3" do
+      seq.path_to_z
+      expect(seq.pos).to eq(3)
+    end
+
+    it "advances to the next Z when called a second time" do
+      seq.path_to_z
+      expect(seq.path_to_z).to eq(%w{
+        22B
+        22C
+        22Z
+      })
+      expect(seq.pos).to eq(6)
+    end
+  end
+
+end
+
+RSpec.describe GhostNavigator do
+  subject(:navigator) {described_class.new(input)}
+  
+  let(:input) {
+    <<~MAP
+      LR
+
+      11A = (11B, XXX)
+      11B = (XXX, 11Z)
+      11Z = (11B, XXX)
+      22A = (22B, XXX)
+      22B = (22C, 22C)
+      22C = (22Z, 22Z)
+      22Z = (22B, 22B)
+      XXX = (XXX, XXX)
+    MAP
+  }
+
+  it "can find the path to a node ending z" do
+    expect(navigator.steps_to_all_be_at_z).to eq(6)
+  end
 
 end
