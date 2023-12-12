afb5d6dd049f8763af8887ad9ed22cde9841bd12
David Heath <david.heath@digital.cabinet-office.gov.uk>
2023-12-12 22:05:32 +0000

    convert tabs to spaces

--- lib/day9.rb
+++ lib/day9.rb
@@ -1,37 +1,37 @@
 class SequenceAnalyzer
-	attr_reader :sequence
+  attr_reader :sequence
 
-	def initialize(sequence)
-		@sequence = sequence
-	end
+  def initialize(sequence)
+    @sequence = sequence
+  end
 
-	def self.differential(seq)
-		seq.each_cons(2).map {|a,b| b-a}
-	end
+  def self.differential(seq)
+    seq.each_cons(2).map {|a,b| b-a}
+  end
 
-	def differentials
-		return @differentials if @differentials
+  def differentials
+    return @differentials if @differentials
 
-		@differentials = []
-		s = sequence
+    @differentials = []
+    s = sequence
 
-		begin
-			s = self.class.differential(s)
-			@differentials << s
-		end while !s.all? {|n| n==0}
+    begin
+      s = self.class.differential(s)
+      @differentials << s
+    end while !s.all? {|n| n==0}
 
-		@differentials
-	end
+    @differentials
+  end
 
-	def extrapolate
-		differentials.inject(sequence.last) do |memo, differential|
-			memo + differential.last
-		end
-	end
+  def extrapolate
+    differentials.inject(sequence.last) do |memo, differential|
+      memo + differential.last
+    end
+  end
 
-	def self.from_file(data)
-		data.split("\n").map {|line| SequenceAnalyzer.new(line.split(" ").map(&:to_i))}
-	end
+  def self.from_file(data)
+    data.split("\n").map {|line| SequenceAnalyzer.new(line.split(" ").map(&:to_i))}
+  end
 end
 
 if __FILE__==$0
